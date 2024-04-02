84d8ef8e83338ad47532a22e7f255f392a6298f3
Rishabh Thakur <rishabhthakur046@gmail.com>
2024-04-02 23:37:21 +0530

    project landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Alegreya } from "next/font/google";
+import "./globals.css";
+
+const roboto = Alegreya({
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Fly High",
+  description:
+    "Get inspired by the work of millions of top-rated designers & agencies around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
